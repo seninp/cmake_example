de83db237e0f06d4adabbaa9a9e4bd45856b2b64
Pavel Senin <seninp@gmail.com>
2017-12-02 07:11:13 +0100

    adding mult into test

--- tests/test.py
+++ tests/test.py
@@ -3,3 +3,8 @@ import cmake_example as m
 assert m.__version__ == '0.0.1'
 assert m.add(1, 2) == 3
 assert m.subtract(1, 2) == -1
+
+from cmake_example.mult import mult
+assert mult(2, 2) == 4
+
+assert False
